85caaae95fc39fff3d2a7736b3f24911ba3db859
sinanseyhan <seyhan.sinan@gmail.com>
2022-05-28 14:51:14 +0200

    Exports

--- green-electricity-project/exports.py
+++ green-electricity-project/exports.py
@@ -8,17 +8,23 @@ class Exports():
 
     def get_exports(self):
         '''
-        Function that gives the cleaned Power Plants Database
-
+        Load the complete export csv file.
         '''
         EXPORTS_PATH = '../raw_data/ElecExportsByPartnerCountry.csv'
-        COUNTRIES_PATH = '../raw_data/CountryCodes.csv'
         temp = pd.read_csv(EXPORTS_PATH)
+        return temp
+
+    def get_eu_exports(self):
+        '''
+        Function that gives the EU countries exports.
+        '''
+        COUNTRIES_PATH = '../raw_data/CountryCodes.csv'
+        temp = Exports().get_exports()
 
         # 27 EU Countries
         EU_Countries = ['Belgium', 'Bulgaria', 'Czechia', 'Denmark', 'Germany', 'Estonia', 'Ireland',
                         'Greece', 'Spain', 'France', 'Croatia', 'Italy', 'Cyprus', 'Latvia', 'Lithuania',
-                        'Luxembourg', 'Hungary', 'Malta', 'Netherlands', 'Austria', 'Poland', 'Portugal',
+                        'Luxembourg', 'Hungary', 'Malta', 'Netherlands (the)', 'Austria', 'Poland', 'Portugal',
                         'Romania', 'Slovenia', 'Slovakia', 'Finland', 'Sweden']
 
         countries = pd.read_csv(COUNTRIES_PATH, encoding = 'unicode_escape')
@@ -62,16 +68,6 @@ class Exports():
 
         exports = exports[exports['EU?']==True]
 
-        #exports.rename({'Country_y':'Partner_country', 'Country_x': 'Country', 'Alpha_3_code_x':'Alpha_3_code'}, axis=1, inplace=True)
-        #exports.drop(columns=['siec', 'EU?', 'Alpha_3_code_y', 'Numeric'], inplace=True)
-
-        # Getting the final structure of the dataset
-        # exports = exports[['partner', 'Partner_country', 'unit', 'Country', 'Alpha_2_code', 'Alpha_3_code',
-        #                 '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998',
-        #                 '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007',
-        #                 '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016',
-        #                 '2017', '2018', '2019', '2020']]
-
         # Downcasting the numerical values
         years = ['1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998',
                 '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007',
@@ -81,3 +77,11 @@ class Exports():
             exports[year] = pd.to_numeric(exports[year], errors='raise', downcast='float')
 
         return exports
+
+    def get_total_exports(self):
+        '''
+        Gives total exports per country
+        '''
+        exports = Exports().get_exports()
+        exports_grouped = exports.groupby(by='country').sum()
+        return exports_grouped
