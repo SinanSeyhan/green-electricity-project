32f289a508f4aa7182ee47d165031c7cebd0f897
sinanseyhan <seyhan.sinan@gmail.com>
2022-05-31 17:57:56 +0200

    Added EU Democracy Index

--- green-electricity-project/utils.py
+++ green-electricity-project/utils.py
@@ -27,7 +27,24 @@ class DemocracyIndex():
 
         EU?: Whether the country is in EU27
         '''
+
+        pd.set_option('mode.chained_assignment', None)
         DEMOCRACY_INDEX_PATH = '../raw_data/Democracy_Index.csv'
         df = pd.read_csv(DEMOCRACY_INDEX_PATH)
 
         return df
+
+    def get_eu_democracy_index(self):
+        '''
+        Gives the democracy index of the 27 EU countries.
+        Data taken from: https://en.wikipedia.org/wiki/Democracy_Index
+        Country: Name of the country
+
+        Regime Type: Type of democracy in the country
+        '''
+
+        temp = DemocracyIndex().get_democracy_index()
+        df_eu = temp[temp['EU?']==True]
+        df_eu.drop(columns=['EU?'], axis=1, inplace=True)
+
+        return df_eu
