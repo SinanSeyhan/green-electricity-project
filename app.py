247fb6739366357bc96271b398cbb4f34dc36cac
sinanseyhan <seyhan.sinan@gmail.com>
2022-06-04 11:18:44 +0200

    Streamlit Update

--- app.py
+++ app.py
@@ -14,3 +14,8 @@ st.markdown(''' ***Exports*** ''')
 exports_module = importlib.import_module("green-electricity-project.exports", package=True).Exports()
 df_exp = exports_module.get_total_eu_exports()
 df_exp
+
+st.markdown(''' ***Model*** ''')
+validate_module = importlib.import_module("green-electricity-project.predict", package=True).Prediction()
+df_exp = validate_module.validate_EU_countries()
+df_exp
