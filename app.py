4059bff659e4071ed0be452b94900812a6953cdc
Armin Zoerkler <armin.zoerkler@gmail.com>
2022-06-09 22:09:15 +0200

    resolved issues

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from json import load
 import streamlit as st
 import importlib
 import pandas as pd
@@ -14,127 +15,36 @@ st.markdown("""---""")
 ###########################################
 
 st.title('Consumption')
-consumption_module = importlib.import_module(
-    "green-electricity-project.consumption_viz_and_pred", package=True)
-
-load_prepared_predictions = True
-
-if load_prepared_predictions == False:
-    option_cons = st.selectbox(
-        'Select a country',
-        ('EU', 'Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic',
-        'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary',
-        'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta',
-        'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia',
-        'Spain', 'Sweden'),
-        key='option_cons')
-
-if run:
-    info = st.empty()
-    info.write('Predicting the future of electricity consumption...')
-
-    consumption = consumption_module.ConsumptionVaP(option_cons)
-    consumption.run_viz_and_pred(load_prep = True)
-
-    st.markdown(''' ****Historic Consumption 1990 - 2020**** ''')
-
-    st.plotly_chart(consumption.fig, use_container_width=True)
-
-    st.markdown(''' ****Prediction of selected consumption category**** ''')
-
-    real_tot_cons_2019 = round(
-        consumption.consumption_data.sum()['2015':'2019'].mean())
-    pred_tot_cons_2030 = round(consumption.total_future_consumption[40])
-
-    rise_perc = round(
-        (pred_tot_cons_2030 - real_tot_cons_2019) / real_tot_cons_2019 * 100,
-        2)
-
-    if option_cons in ['Czech Republic', 'Netherlands', 'EU']:
-        st.markdown(
-            f'Until 2030 the total energy consumption of the <font color="red"><b>{option_cons}</b></font> will change by about',
-            unsafe_allow_html=True)
-    else:
-        st.markdown(
-            f'Until 2030 the total energy consumption of <font color="red"><b>{option_cons}</b></font> will change by about',
-            unsafe_allow_html=True)
-
-    st.markdown('<style>.big-font {font-size:50px !important;}</style>',
-                unsafe_allow_html=True)
-    st.markdown(
-        f'<p class="big-font"><font color="red"><b>{rise_perc:+} %</b></font></p>',
-        unsafe_allow_html=True)
-
-    st.plotly_chart(consumption.fig_pred, use_container_width=True)
-
-
-
-st.markdown("""---""")
-###########################################
-
-###############################
-## PRODUCTION ##
-###############################
-st.title('Production')
-# path = '../raw_data/Production_Cleaned.csv'
-# production_module = importlib.import_module("green-electricity-project.production_viz", package=True).EuElecProduction()
-# st.plotly_chart(production_module.GEP_pred_vs_Actual(), sharing='streamlit')
-st.markdown("""---""")
-###############################
-## ENERGY MIX ##
-###############################
-
-
-st.title('Energy Mix of Countries')
-power_module = importlib.import_module("green-electricity-project.powerplants", package=True).PowerPlants()
-df = power_module.get_eu_power_plants()
-# Create dropdown menu:
-countries = tuple(df.country)
-option = st.selectbox(label='Please select the Country you want to see: ', options=countries)
-
-# Total generation capacity:
-capa = df[df['country'] == option]['total_gw_calculated'].values[0]
-st.subheader(f'Total capacity of **{option}**: ***{round(capa, 2)}*** GW')
-
-# Pie Chart
-st.header(f"{option}'s Energy Mix in Electricity")
-st.plotly_chart(power_module.plot_eu_mix(option), sharing='streamlit')
-st.markdown("""---""")
-
-###########################################
-=======
-
-st.title('Consumption')
 
 consumption_module = importlib.import_module(
     "green-electricity-project.consumption_viz_and_pred", package=True)
 
 load_prepared_predictions = True
-
+run = False
 if load_prepared_predictions == False:
     option_cons = st.selectbox(
         'Select a country',
-        ('EU', 'Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic',
-        'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary',
-        'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta',
-        'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia',
-        'Spain', 'Sweden'),
+        ('EU', 'Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus',
+         'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France',
+         'Germany', 'Greece', 'Hungary', 'Ireland', 'Italy', 'Latvia',
+         'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 'Poland',
+         'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden'),
         key='option_cons')
->>>>>>> master
 
-<<<<<<< HEAD
     run = st.button("Predict Future Consumption")
 
-<<<<<<< HEAD
-###########################################
-## GEOLOCATION POWER PLANTS ##
-###########################################
-=======
     if run:
         info = st.empty()
         info.write('Predicting the future of electricity consumption...')
-        consumption = consumption_module.ConsumptionVaP(option_cons)
-        consumption.run_viz_and_pred(info)
+
+elif load_prepared_predictions == True:
+    option_cons = 'EU'
+
+if load_prepared_predictions or run:
+
+    consumption = consumption_module.ConsumptionVaP(option_cons)
+    #@st.cache
+    consumption.run_viz_and_pred(load_prep=load_prepared_predictions)
 
     st.markdown(''' ****Historic Consumption 1990 - 2020**** ''')
 
@@ -169,6 +79,8 @@ if load_prepared_predictions == False:
 
 
 st.markdown("""---""")
+
+
 ###########################################
 
 ###############################
